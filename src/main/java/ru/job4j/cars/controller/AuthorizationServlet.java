5f48c71cd6245b3695cfac37121730b81ae9fc47
Lyubov <n.lubovj@gmail.com>
2024-02-12 23:51:32 +0300

    Fix authorization

--- src/main/java/ru/job4j/cars/controller/AuthorizationServlet.java
+++ src/main/java/ru/job4j/cars/controller/AuthorizationServlet.java
@@ -1,5 +1,6 @@
 package ru.job4j.cars.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.job4j.cars.model.User;
 import ru.job4j.cars.service.UserService;
 
@@ -10,8 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+
 public class AuthorizationServlet extends HttpServlet {
     private final UserService userService = UserService.instOf();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("logOut") != null) {
+            HttpSession sc = req.getSession();
+            sc.setAttribute("user", null);
+            return;
+        }
+        User user = (User) req.getSession().getAttribute("user");
+        if (user == null) {
+            resp.setStatus(SC_UNAUTHORIZED);
+        } else {
+            resp.setStatus(SC_OK);
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonFromUser = objectMapper.writeValueAsString(user);
+            resp.getWriter().write(jsonFromUser);
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -19,7 +41,7 @@ public class AuthorizationServlet extends HttpServlet {
         if (user != null && user.getPassword().equals(req.getParameter("password"))) {
             HttpSession sc = req.getSession();
             sc.setAttribute("user", user);
-            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setStatus(SC_OK);
         } else {
             resp.sendError(422, "Invalid username or password");
         }
